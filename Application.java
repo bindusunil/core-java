4a27fdea06f11b1f14da18086bfe3b29d472e8de
bindusunil <bindusunil.xworkz@gmail.com>
2024-06-24 14:44:49 +0530

    1st program

---
+++ Application.java
@@ -0,0 +1,21 @@
+public class Application{
+public static String name1 = "watsapp";
+public static int rating = 4;
+public static double updatesize = 36.02;
+public static String offeredby = "watsapp llc";
+public static String releasedon = "oct 18 2010";
+public static long numberofusers = 5000000000L;
+public static String updateddate = "jun 17,2024";
+public static boolean dididownload = true;
+public static int numberofhoursused = 3;
+public static void main(String args[]){
+String name2 = "instagram";
+int rating = 4;
+String updatedon = "jin 17,2024";
+String offeredby = "instagram";
+String releasedon1 = "apr 3,2012";
+String version = "336.0.0.35.90";
+String compatability = "works on all devices";
+boolean dididownload = true;
+}
+}
